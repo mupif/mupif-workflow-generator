1afee78e5446791021d7414c0833c915d1572fa9
Stanislav <sulcstanda@seznam.cz>
2020-01-20 00:44:25 +0100

    Field export to image added to example02.

--- workfloweditor/examples/example02_tm_cantilever/field_to_vtk.py
+++ workfloweditor/examples/example02_tm_cantilever/field_to_vtk.py
@@ -42,3 +42,44 @@ class field_export_to_VTK(mupif.Application.Application):
     def getCriticalTimeStep(self):
         return mupif.Physics.PhysicalQuantities.PhysicalQuantity(1000.0, 's')
 
+
+class field_export_to_image(mupif.Application.Application):
+    def __init__(self):
+        mupif.Application.Application.__init__(self)
+        self.field = None
+        self.step_number = 0
+        self.filename_base = "VTKField"
+        self.metadata.update({'Name': 'field_export_to_image',
+                              'ID': 'simpleImageExport',
+                              'Version_date': '1.0 1.1.2020',
+                              'Inputs': [
+                                  {'Name': 'field', 'Type': 'mupif.Field', 'Required': False,
+                                   'Type_ID': 'mupif.FieldID.FID_Temperature'}
+                              ],
+                              'Outputs': []})
+
+    def initialize(self, file='', workdir='', executionID='', metaData={}, validateMetaData=False, **kwargs):
+        pass
+
+    def setField(self, field, objectID=0):
+        """
+
+        :param mupif.Field.Field field:
+        :param objectID:
+        :return:
+        """
+        self.field = field
+
+    def solveStep(self, tstep, stageID=0, runInBackground=False):
+        if self.field:
+            if not self.step_number:
+                if self.field.fieldID == mupif.FieldID.FID_Temperature:
+                    self.filename_base = "Fig_Temperature"
+                if self.field.fieldID == mupif.FieldID.FID_Displacement:
+                    self.filename_base = "Fig_Displacement"
+
+            self.step_number += 1
+            self.field.field2Image2D(fileName='%s_%d' % (self.filename_base, self.step_number))
+
+    def getCriticalTimeStep(self):
+        return mupif.Physics.PhysicalQuantities.PhysicalQuantity(1000.0, 's')
